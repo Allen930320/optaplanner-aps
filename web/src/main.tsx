e6b10b55476f52fc255b0210e727c765eae081eb
Allen <allen.chen@getsmileapi.com>
2025-11-13 17:19:35 +0800

    解决 antd 弹窗兼容性问题

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,6 +5,8 @@ import 'regenerator-runtime/runtime';
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// 导入Ant Design React 19兼容性包
+import '@ant-design/v5-patch-for-react-19';
 import './index.css'
 import App from './App.tsx'
 
